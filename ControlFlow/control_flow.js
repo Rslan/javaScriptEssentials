f841d95d72754b291588983c356be4e6341e6a75
Ruslan <Ruslan@coursera.org>
2024-12-30 03:50:37 -0500

    ternary operator

--- ControlFlow/control_flow.js
+++ ControlFlow/control_flow.js
@@ -48,3 +48,9 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+// -----
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
